222f54df8fc7a0c274e66ac21566316bfc2dd072
TiagoAlmeida <thyagoeap@gmail.com>
2022-02-15 17:17:48 -0300

    Initiate logic

--- app/src/main/java/com/example/calculadoradegorjeta/MainActivity.java
+++ app/src/main/java/com/example/calculadoradegorjeta/MainActivity.java
@@ -3,12 +3,64 @@ package com.example.calculadoradegorjeta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText editValor, editGorjeta, editTotal;
+    private SeekBar seekBar;
+    private TextView textPct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editGorjeta = findViewById(R.id.editGorjeta);
+        editTotal = findViewById(R.id.editTotal);
+        editValor = findViewById(R.id.editValor);
+        seekBar = findViewById(R.id.seekPct);
+        textPct = findViewById(R.id.textPct);
+
+//        editTotal.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//                textPct.setText(s.toString());
+//            }
+//        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textPct.setText(progress+" %");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
